49137eeb575cdaf83d2b167cedd2d4b0a0ccc73e
Henryk <hbblok@wp.pl>
2023-03-30 18:12:08 +0200

    Add project files.

---
+++ Pogoda/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Pogoda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj temperaturę:");
+
+            string x = Console.ReadLine();
+
+            int ax = int.Parse(x);
+
+            if (ax < 0) 
+            {
+                Console.WriteLine("cholernie piździ");
+            
+            }
+            if (ax > 0 && ax < 10) 
+            {
+                Console.WriteLine("zimno");
+
+            
+            }
+            if (ax >= 10 && ax < 20 ) 
+            {
+                Console.WriteLine("chłodno");
+
+            }
+            if(ax >= 20 && ax < 30 ) 
+            {
+                Console.WriteLine("w sam raz");
+
+            }
+            if ( ax >= 30 && ax < 40) 
+            {
+                Console.WriteLine("zaczyna być słabo , bo gorąco");
+
+            }
+            if (ax >= 40) 
+            {
+                Console.WriteLine("a weź wyprowadzam się na Alaskę");
+            }
+
+        }
+    }
+}
